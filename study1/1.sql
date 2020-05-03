2844c2430797627b4c59796ea229e9e0e20a59fa
akira ogawa <oakira@outlook.com>
2020-05-03 18:52:10 +0900

    20200503

--- study1/1.sql
+++ study1/1.sql
@@ -1,89 +1,217 @@
 --mysql起動
-mysql.server start
---mysql停止
-mysql.server stop
---プロセスの確認・終了
-ps aux | grep "mysql"
-sudo kill pid
---初期設定
-mysql_secure_installation
---mysql　パスなしログイン
+mysql.server start --mysql停止
+mysql.server stop --プロセスの確認・終了
+ps aux | grep "mysql" sudo kill pid --初期設定
+mysql_secure_installation --mysql　パスなしログイン
 mysql.server start --skip-grant-tables
 --mysqlログイン
-mysql -u root -p
---mysqlユーザー確認
-select Host, User, Password from mysql.user;
+mysql - u root - p --mysqlユーザー確認
+select
+	Host,
+	User,
+	Password
+from
+	mysql.user;
+
 --mysql セーフモードで起動
 sudo mysqld_safe --skip-grant-tables &
 --ユーザーの作成
-create user 'user名'@'ホスト名' identified by 'パスワード';
+create user 'user名' @'ホスト名' identified by 'パスワード';
+
 --ユーザーの追加と権限付与
-grant all on publications.* to 'xxxxxxxxx'@'localhost' identified by 'xxxxxxxxx';
-grant all on publications.* to 'root'@'localhost' identified by 'Yd2#~yR4';
-grant all on publications.* to 'akiraogawa'@'%' identified by 'Yd2#~yR4';
+grant all on publications.* to 'xxxxxxxxx' @'localhost' identified by 'xxxxxxxxx';
+
+grant all on publications.* to 'root' @'localhost' identified by 'Yd2#~yR4';
+
+grant all on publications.* to 'akiraogawa' @'%' identified by 'Yd2#~yR4';
+
 --無理やりユーザー追加()
-INSERT INTO mysql.user SET user = 'root', host = 'localhost', password = Password('silversun0424');
-DELETE FROM mysql.user WHERE user = 'root' AND host = 'localhost'; 
-INSERT INTO mysql.user SET user = 'root',host = 'localhost', authentication_string=password('silversun0424'),Select_priv = 'y',Insert_priv = 'y',Update_priv = 'y',Delete_priv = 'y',Create_priv = 'y',Drop_priv = 'y',Reload_priv = 'y',Shutdown_priv = 'y',Process_priv = 'y',File_priv = 'y',Grant_priv = 'y',References_priv = 'y',Index_priv = 'y',Alter_priv = 'y',Show_db_priv = 'y',Super_priv = 'y',Create_tmp_table_priv = 'y',Lock_tables_priv = 'y',Execute_priv = 'y',Repl_slave_priv = 'y',Repl_client_priv = 'y',Create_view_priv = 'y',Show_view_priv = 'y',Create_routine_priv = 'y',Alter_routine_priv = 'y',Create_user_priv = 'y',Event_priv = 'y',Trigger_priv = 'y',Create_tablespace_priv = 'y';
+INSERT INTO
+	mysql.user
+SET
+	user = 'root',
+	host = 'localhost',
+	password = Password('silversun0424');
+
+DELETE FROM
+	mysql.user
+WHERE
+	user = 'root'
+	AND host = 'localhost';
+
+INSERT INTO
+	mysql.user
+SET
+	user = 'root',
+	host = 'localhost',
+	authentication_string = password('silversun0424'),
+	Select_priv = 'y',
+	Insert_priv = 'y',
+	Update_priv = 'y',
+	Delete_priv = 'y',
+	Create_priv = 'y',
+	Drop_priv = 'y',
+	Reload_priv = 'y',
+	Shutdown_priv = 'y',
+	Process_priv = 'y',
+	File_priv = 'y',
+	Grant_priv = 'y',
+	References_priv = 'y',
+	Index_priv = 'y',
+	Alter_priv = 'y',
+	Show_db_priv = 'y',
+	Super_priv = 'y',
+	Create_tmp_table_priv = 'y',
+	Lock_tables_priv = 'y',
+	Execute_priv = 'y',
+	Repl_slave_priv = 'y',
+	Repl_client_priv = 'y',
+	Create_view_priv = 'y',
+	Show_view_priv = 'y',
+	Create_routine_priv = 'y',
+	Alter_routine_priv = 'y',
+	Create_user_priv = 'y',
+	Event_priv = 'y',
+	Trigger_priv = 'y',
+	Create_tablespace_priv = 'y';
+
 --パスワードの設定
-set password for 'ユーザー名'@'ホスト名' = password('パスワード');
-set password for 'root'@'localhost' = password('silversun0424');
+set
+	password for 'ユーザー名' @'ホスト名' = password('パスワード');
+
+set
+	password for 'root' @'localhost' = password('silversun0424');
+
 --ユーザーの削除
-drop user 'user名'@'ホスト名 or IPアドレス';
+drop user 'user名' @'ホスト名 or IPアドレス';
+
 --dbの作成
 create database publicartions;
+
 --dbを選択
 use publications;
+
 --データの追加
-create table classics(author VARCHAR(128),title VARCHAR(128),type VARCHAR(16),year CHAR(4)) ENGINE MyISAM;
+create table classics(
+	author VARCHAR(128),
+	title VARCHAR(128),
+	type VARCHAR(16),
+	year CHAR(4)
+) ENGINE MyISAM;
+
 create table tablename (fieldname int unsigned);
+
 create table tablename (fieldname int(4));
+
 create table tablename (fieldname int(4) zerofill);
+
 create table disposble(trash int);
+
 --テーブルの中身を確認する
-select * from classics;
+select
+	*
+from
+	classics;
+
 --列の確認
 describe classics;
+
 --ユーザーの確認
-select user, host from mysql.user;
+select
+	user,
+	host
+from
+	mysql.user;
+
 --既存のテーブルへのIDの追加
-alter table classics add id int unsigned not null auto_increment key;
+alter table
+	classics
+add
+	id int unsigned not null auto_increment key;
+
 --列の削除
-alter table classics drop id;
+alter table
+	classics drop id;
+
 --データ追加
-insert into classics(author,title,type,year)values('マーク・トウェイン','トム・ソーヤの冒険','フィクション','1876');
-insert into classics(author,title,type,year)values('ジェーン・オースティン','傲慢と偏見','フィクション','1811');
-insert into classics(author,title,type,year)values('チャールズ・ダーウィン','種の起源','ノンフィクション','1856');
-insert into classics(author,title,type,year)values('チャールズ・ディケンズ','骨董屋','フィクション','1841');
-insert into classics(author,title,type,year)values('ウィリアム・シェイクスピア','ロミオとジュリエット','演劇','1594');
+insert into
+	classics(author, title, type, year)
+values
+	('マーク・トウェイン', 'トム・ソーヤの冒険', 'フィクション', '1876');
+
+insert into
+	classics(author, title, type, year)
+values
+	('ジェーン・オースティン', '傲慢と偏見', 'フィクション', '1811');
+
+insert into
+	classics(author, title, type, year)
+values
+	('チャールズ・ダーウィン', '種の起源', 'ノンフィクション', '1856');
+
+insert into
+	classics(author, title, type, year)
+values
+	('チャールズ・ディケンズ', '骨董屋', 'フィクション', '1841');
+
+insert into
+	classics(author, title, type, year)
+values
+	('ウィリアム・シェイクスピア', 'ロミオとジュリエット', '演劇', '1594');
+
 --テーブル内容の表示
-select * from classics;
+select
+	*
+from
+	classics;
+
 --テーブル名の変更
-alter table classics rename pre1900;
+alter table
+	classics rename pre1900;
+
 --列の型変更
-alter table classics modify year smallint;
+alter table
+	classics
+modify
+	year smallint;
+
 --列の追加
-alter table classics add pages smallint unsigned;
+alter table
+	classics
+add
+	pages smallint unsigned;
+
 --列名の変更
-alter table  classics change type category VARCHAR(16); 
+alter table
+	classics change type category VARCHAR(16);
+
 --高速検索用のindexを追加する
-alter table classics add index(author(20));
+alter table
+	classics
+add
+	index(author(20));
+
 create index author on classics (author(20));
-alter table classics add index(title(20));
-alter table classics add index(category(4));
-alter table classics add index(year);
+
+alter table
+	classics
+add
+	index(title(20));
+
+alter table
+	classics
+add
+	index(category(4));
+
+alter table
+	classics
+add
+	index(year);
+
 --基本構文
 --DDL
-create
-drop
-alter
+create drop alter
 --DML
-select
-insert
-update
-delete
+select insert update delete
 --DCL
-commit
-rollback
-grant
-revoke
+commit rollback grant revoke
--- study1/2.sql
+++ study1/2.sql
@@ -1,8 +1,111 @@
-create table Shohin 
-(shohin_id CHAR(4) NOT NULL,
-shohin_mei VARCHAR(100) NOT NULL,
-shohin_bunrui VARCHAR(32) NOT NULL,
-hanbai_tabka INTEGER ,
-shiire_tanka INTEGER ,
-torokubi DATE ,
-PRIMARY KEY (shohin_id));
+--table追加
+create table Shohin (
+	shohin_id CHAR(4) NOT NULL,
+	shohin_mei VARCHAR(100) NOT NULL,
+	shohin_bunrui VARCHAR(32) NOT NULL,
+	hanbai_tabka INTEGER,
+	shiire_tanka INTEGER,
+	torokubi DATE,
+	PRIMARY KEY (shohin_id)
+);
+
+--table削除
+drop table Shohin: --tableの定義の変更
+--列を追加
+alter table
+	Shohin
+add
+	column shohin_mei_kana VARCHAR(100);
+
+--列を削除
+alter table
+	Shohin drop column shohin_mei_kana;
+
+--DMLデータ登録
+-- begin transaction; --postgres
+start transaction;
+
+insert into
+	Shohin
+values
+	('0001', 'Tシャツ', '衣服', 1000, 500, '2009-09-20');
+
+insert into
+	Shohin
+values
+	('0002', '穴あけパンチ', '事務用品', 500, 320, '2009-09-11');
+
+insert into
+	Shohin
+values
+	('0003', 'カッターシャツ', '衣服', 4000, 2800, NULL);
+
+insert into
+	Shohin
+values
+	('0004', '包丁', 'キッチン用品', 3000, 2800, '2009-09-20');
+
+insert into
+	Shohin
+values
+	(
+		'0005',
+		'圧力鍋',
+		'キッチン用品',
+		6000,
+		5000,
+		'2009-01-15'
+	);
+
+insert into
+	Shohin
+values
+	(
+		'0006',
+		'フォーク',
+		'キッチン用品',
+		500,
+		NULL,
+		'2009-09-20'
+	);
+
+insert into
+	Shohin
+values
+	(
+		'0007',
+		'おろしがね',
+		'キッチン用品',
+		880,
+		790,
+		'2008-04-28'
+	);
+
+insert into
+	Shohin
+values
+	('0008', 'ボールペン', '事務用品', 100, NULL, '2009-01-11');
+
+--table名の変更
+-- alter table Sohin rename to Shohin; --postgres
+rename table Sohin to Shohin;
+
+--table作成
+create table Jyushoroku(
+	toroku_bango INTEGER NOT NULL,
+	namae VARCHAR(128) NOT NULL,
+	jyusho VARCHAR(256) NOT NULL,
+	tel_no CHAR(10),
+	mail_adress CHAR(20)
+);
+
+--table add primarykey
+alter table
+	Jyushoroku
+add
+	PRIMARY KEY(toroku_bango);
+
+alter table
+	Jyushoroku
+add
+	column yubin_bango CHAR(8) NOT NULL;
